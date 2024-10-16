81bf35f35f9a996235357f680aa341ecaddec482
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2024-10-16 08:08:08 +0000

    react-hot-button

--- react-hot-button/my-app/src/App.tsx
+++ react-hot-button/my-app/src/App.tsx
@@ -1,16 +1,42 @@
 import { useState } from 'react';
+import { ButtonContainer } from './ButtonContainer';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const buttonLabel = ['Hot Button'];
+
+export function App() {
+  const [click, setClick] = useState(0);
+
+  function handleButtonClick() {
+    setClick(click + 1);
+  }
+
+  function getButton() {
+    if (click < 3) {
+      return 'hot-button cold';
+    } else if (click < 6) {
+      return 'hot-button cool';
+    } else if (click < 9) {
+      return 'hot-button tepid';
+    } else if (click < 12) {
+      return 'hot-button warm';
+    } else if (click < 15) {
+      return 'hot-button hot';
+    } else if (click < 18) {
+      return 'hot-button nuclear';
+    } else {
+      return 'hot-button atomic';
+    }
+  }
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <ButtonContainer
+        onClick={handleButtonClick}
+        className={getButton()}
+        label={buttonLabel}
+        index={click}
+      />
     </>
   );
 }
