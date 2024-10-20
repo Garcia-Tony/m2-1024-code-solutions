bc0488e452f6546836201c4332f27593a32f446a
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2024-10-20 23:22:21 +0000

    react-toggle-switch

--- react-toggle-switch/my-app/src/App.tsx
+++ react-toggle-switch/my-app/src/App.tsx
@@ -1,35 +1,20 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
+import { ToggleSwitch } from './ToggleSwitch';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+const label = ['ON', 'OFF'];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+export function App() {
+  const [click, setClick] = useState(0);
+
+  function getButton() {
+    if (click >= label.length - 1) {
+      setClick(0);
+    } else {
+      setClick(click + 1);
+    }
+  }
+
+  return <ToggleSwitch onClick={getButton} label={label} index={click} />;
 }
-
 export default App;
