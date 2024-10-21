42e00f77db3aeef47ecd5438e88c813b8c2dc17e
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2024-10-21 01:35:30 +0000

    react-lists

--- react-lists/my-app/src/App.tsx
+++ react-lists/my-app/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../../../../../vite.svg';
+import { PokemonList } from './list';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
 
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' },
+];
+
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Pokedex</h1>
+      <PokemonList pokedex={pokedex} />
+    </div>
   );
-}
+};
 
 export default App;
--- react-lists/react-lists-quiz-notes.md
+++ react-lists/react-lists-quiz-notes.md
@@ -5,14 +5,19 @@
 Answer the following questions in the provided markdown file before turning in this exercise:
 
 - When would we want to programmatically create a list of React components?
+We programmatically create a list of React components when we have dynamic data that can change over time, allowing us to render components based on that data, such as lists of items, users, or products.
 
 - Why is it important for React components to be data-driven?
+Data-driven components ensure that the UI reflects the current state of the application, making it easier to manage updates and maintain consistency across the application.
 
 - What `Array` method is commonly used to create a list of React components?
+The map() method
 
 - Why do components in a list need to have unique keys?
+Unique keys help React identify which items have changed, are added, or are removed, improving the performance of the rendering process and ensuring that the correct component state is maintained during updates.
 
 - What is the best value to use as a "key" prop when rendering lists?
+The best value for a "key" prop is a unique identifier for each item in the list, such as an ID from a database. This ensures that keys remain stable across renders.
 
 ## Notes
 
